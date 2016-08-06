7755581e3d47d29390be3543ae8b237b101f574f
mdelgolla <mdelgolla@gmail.com>
2016-08-06 23:59:20 +0530

    Dela
    repace

--- admin/index.php
+++ admin/index.php
@@ -7,20 +7,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Integrated Fine Payment System 
+    <title>Integrated Fine Payment System
     </title>
 
     <link href="css/bootstrap.min.css" rel="stylesheet" />
     <link href="css/modern-business.css" rel="stylesheet" />
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" />
-	<link href="font-awesome/css/style.css" rel="stylesheet" />
+    <link href="font-awesome/css/style.css" rel="stylesheet" />
 
 </head>
 
 <body>
 
 
-	<div class="container-fluid">
+<div class="container-fluid">
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
             <div class="navbar-header">
@@ -30,17 +30,16 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="driver/index.php">Driver Login</a>
-                <a class="navbar-brand" href="login/login.php">Police Login</a>
+                <a class="navbar-brand" href="login.html">Log In</a>
             </div>
-           
+
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li>
-                        <a href="index.html">Home</a>
+                        <a href="index.php">Home</a>
                     </li>
-					<li>
-                        <a href="about.html">About</a>
+                    <li>
+                        <a href="about.php">About</a>
                     </li>
                     <li>
                         <a href="services.html">Services</a>
@@ -48,39 +47,36 @@
                     <li>
                         <a href="contacts.html">Contact</a>
                     </li>
-                    </ul>
-                </div>
+                </ul>
             </div>
-        </nav>
+        </div>
+    </nav>
 
-<form>
-  <h1>Driver Log in</h1>
-  <div class="inset">
-  <p>
-    <label for="LicenceNo">LICENCE NO</label>
-    <input type="text" name="LicenceNo" id="LicenceNo">
-  </p>
-  <p>
-    <label for="email">EMAIL ADDRESS</label>
-    <input type="text" name="email" id="email">
-  </p>
-  <p>
-    <label for="password">PASSWORD</label>
-    <input type="password" name="password" id="password">
-  </p>
-  <p>
-    <input type="checkbox" name="remember" id="remember">
-    <label for="remember">Remember me for 14 days</label>
-  </p>
-  </div>
-  <p class="p-container">
-    <span>Forgot password ?</span>
-    <!--input button type="submit" name="go" id="go" value="Log in"-->	
-	<a href="form.html" class="btn btn-default">Log In</a>
-	
-	
-  </p>
-</form>
+    <form>
+        <h1>Driver Log in</h1>
+        <div class="inset">
+            <p>
+                <label for="Usernane">Username</label>
+                <input type="text" name="uname" id="uid">
+            </p>
+            
+            <p>
+                <label for="password">PASSWORD</label>
+                <input type="password" name="password" id="password">
+            </p>
+            <p>
+                <input type="checkbox" name="remember" id="remember">
+                <label for="remember">Remember me for 14 days</label>
+            </p>
+        </div>
+        <p class="p-container">
+            <span>Forgot password ?</span>
+            <!--input button type="submit" name="go" id="go" value="Log in"-->
+            <a href="form.html" class="btn btn-default">Log In</a>
+
+
+        </p>
+    </form>
 </div>
 </body>
 </html>
